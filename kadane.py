860fa3f59d0a2f472cb8cc9a76dea4ce678fdee5
AKHIL PRATAP SINGH <as1575828@gmail.com>
2025-11-07 21:57:24 +0530

    Update kadane.py

--- kadane.py
+++ kadane.py
@@ -4,7 +4,7 @@ def maxS(arr):
     max_sum=arr[0]
     curr_sum=arr[0]
     for i in range(1,len(arr)):
-        curr_sum=max(arr[i], curr_sum+arr[i])
-        max_sum=max(max_sum, curr_sum)
+        curr_sum=max(arr[i],curr_sum+arr[i])
+        max_sum=max(max_sum,curr_sum)
     return max_sum
 print(maxS(arr))
