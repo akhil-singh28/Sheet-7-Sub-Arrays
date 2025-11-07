c8c5ac9966b09cb41f558491fc6ffa0c43f7af0b
AKHIL PRATAP SINGH <as1575828@gmail.com>
2025-11-07 21:58:35 +0530

    Update sumofeverysingle.py

--- sumofeverysingle.py
+++ sumofeverysingle.py
@@ -2,6 +2,6 @@ n=int(input())
 arr=list(map(int,input().split()))
 for i in range(n):
     total=0
-    for j in range(i, n):
+    for j in range(i,n):
         total += arr[j]
         print(total)
